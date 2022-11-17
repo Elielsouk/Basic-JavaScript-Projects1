cd1bbbfc8cd9e1652439865260dcd594d6d3cc67
Elielso Francisco <107587443+Elielsouk@users.noreply.github.com>
2022-11-17 15:56:32 +0000

    Basic button project.
    In this project I show how to change text and some result of Math calculation using += operators.

---
+++ main.js
@@ -0,0 +1,10 @@
+function myFunction1() { //This is a function.
+  document.getElementById("demo1").innerHTML = "Hello guys, This is Elielso Francsico";//document.getElementId to change the Text.
+  var x1 = document.getElementById("demo1");
+  x1.style.color = "blue"; //Get the result with color text "blue".
+  
+  let x = 15;// Assign the value 15 to x
+let y = 8;// Assign the value 8 to y
+let z = x + y;// Assign the value x + y to z
+document.getElementById("demo").innerHTML = "The sum of x + y is: " + z;// Display z
+  }
